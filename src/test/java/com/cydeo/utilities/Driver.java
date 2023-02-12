b7d86fc29752768f9d01c98331e2ab33af7a05be
suhedapehlivan <suheda.90@gmail.com>
2023-02-12 23:20:17 +0300

    cucumber setup

---
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -0,0 +1,109 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+
+    /*
+    Creating a private constructor, so we are closing
+    access to the object of this class from outside the class
+     */
+    private Driver(){}
+
+    /*
+    We make WebDriver private, because we want to close access from outside the class.
+    We make it static because we will use it in a static method.
+     */
+    private static WebDriver driver; // value is null by default
+
+    /*
+    Create a re-usable utility method which will return same driver instance when we call it
+     */
+    public static WebDriver getDriver(){
+
+        if (driver == null){
+
+            /*
+            We read our browserType from configuration.properties.
+            This way, we can control which browser is opened from outside our code, from configuration.properties.
+             */
+            String browserType = ConfigurationReader.getProperty("browser");
+
+
+            /*
+                Depending on the browserType that will be return from configuration.properties file
+                switch statement will determine the case, and open the matching browser
+            */
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+
+            }
+        }
+
+        return driver;
+
+    }
+
+    /*
+    This method will make sure our driver value is always null after using quit() method
+     */
+    public static void closeDriver(){
+        if (driver != null){
+            driver.quit(); // this line will terminate the existing session. value will not even be null
+            driver = null;
+        }
+    }
+}
+/*
+session_id for driver : "driver" --> driver_1209381203987askdf34098
+
+driver.get
+driver.findELement
+driver.maximize
+driver.method
+driver.quit(); --> driver_1209381203987askdf34098 --> session_id will be deleted, terminated, erased
+
+driver_session_id: driver_123098123019328adsf123
+
+How to handle Closing or Quitting driver with Driver utility class?
+
+--> When we created a new .getDriver() method in Driver utility class and implemeted Singleton design pattern.
+
+--> This design pattern requires my driver to be "driver == null (true)" to be able to generate a new driver.
+
+--> When we use default driver.quit() method that is coming from Selenium library, we terminate the existing driver session completely.
+
+--> This creates issue for our existing structure. Since driver is not null or session is completely deleted/terminated, we cannot continue with our execution of following tests.
+
+--> To solve this issue, we created Driver.closeDriver() method.
+--> In this method 2 thing are happening:
+    #1- We use driver.quit() to terminate the session and close browsers.
+    #2- Set the driver session value back to "null", so rest of our tests can be executed.
+
+
+ */
