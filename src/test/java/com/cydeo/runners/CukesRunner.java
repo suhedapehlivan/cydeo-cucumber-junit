25ba2015fb5e408b55e4dd966a7abed9b3f6296c
suhedapehlivan <suheda.90@gmail.com>
2023-02-15 00:38:55 +0300

    day14

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions"
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = "@employee and @admin"
 )
 public class CukesRunner {
 }
