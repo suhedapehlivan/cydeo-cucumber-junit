0e9a624fa3e5274dd7503877768a1defc8a9f3e4
suhedapehlivan <suheda.90@gmail.com>
2023-02-23 00:45:23 +0300

    day17

--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
@@ -1,7 +1,14 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.DropdownsPage;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTable_StepDefinitions {
@@ -11,6 +18,42 @@ public class DataTable_StepDefinitions {
         System.out.println(fruits);
         System.out.println(fruits.get(1));
     }
+
+
+
+
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+
+        Driver.getDriver().get("https://practice.cydeo.com/dropdown");
+    }
+
+
+
+    DropdownsPage dropdownsPage=new DropdownsPage();
+
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
+
+        Select select=new Select(dropdownsPage.monthDropdown);
+
+        //List of all ACTUAL month <options> as a web element
+        List<WebElement> actualOptionAsWebElement = select.getOptions();
+
+        //create a list of string and pass all of the actual web element option' string into that new list
+
+        //**** we get the web element, and getting the text of it, and put in text as a string into the specific list.
+        //List of all ACTUAL month <options> as a string
+        List<String> actualOptionsAsString = new ArrayList<>();
+        for (WebElement each :actualOptionAsWebElement) {
+            actualOptionsAsString.add(each.getText());
+        }
+        Assert.assertEquals(expectedMonths, actualOptionsAsString);
+        // Assert will check the size of the lists first. If it is matching it will check content 1 by 1.
+    }
+
+
+
 
 
 }
