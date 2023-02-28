675b920403d90dde2dff15a24cfc970cdab62ca9
suhedapehlivan <suheda.90@gmail.com>
2023-02-28 14:44:39 +0300

    day19

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-reports.html",
+        plugin = {//"prettiy",
+                "html:target/cucumber-reports.html",
                   "rerun:target/rerun.txt",
                   "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
         features = "src/test/resources/features",
